3c11441afb1f2462667d066b16d15fe78fb59ee6
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-10-06 15:57:04 +0300

    1star project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
+var debounce = require('lodash.debounce');
+const refs={
+    inputSearchRef: document.querySelector('#search-box'),
+    listCountryRef: document.querySelector('.country-list'),
+    infoCountryBoxRef: document.querySelector('.country-info'),
+
+}
 
 const DEBOUNCE_DELAY = 300;
+const BASE_URL="https://restcountries.com/v3.1/"
+function searchCountry(city) {
+    return fetch(`${BASE_URL}name/${city}`).then(
+      response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      }
+    ).then( response => {
+        console.log(response)
+      })
+  }
+  refs.inputSearchRef.addEventListener('input',debounce((event)=>{
+    searchCountry(event.target.value)
+  },300))
+ 
